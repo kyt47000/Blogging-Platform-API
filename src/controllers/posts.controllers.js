706cd5e3d74c33e494fc8eaf4184ade13ed39f9b
Yash Kayastha <kyt47000@gmail.com>
2025-06-30 15:50:43 +0530

    Update posts.controllers.js

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -3,7 +3,7 @@ import BlogPost from "../models/blogPost.model.js";
 export const createPosts = async (req, res) => {
   try {
     const blog = await BlogPost.create(req.body);
-    res.status(200).json(blog);
+    res.status(201).json(blog);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -33,7 +33,7 @@ export const deletePostById = async (req, res) => {
       return res.status(404).json({ error: "Post not found" });
     }
 
-    res.status(200).json(deletePost);
+    res.status(204).json(deletePost);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
